009644d32bcb763773c7693f24d85b23e38b0008
Henrique Coelho <henriquemacedocoelho@hotmail.com>
2024-06-29 22:36:00 +0000

    Creating sources.yml and stg/int models for dim_address

---
+++ models/intermediate/int_adventure_works__address.sql
@@ -0,0 +1,39 @@
+with
+    address_info as (
+        select * 
+        from {{ ref('stg_adventure_works__address') }}
+    )
+
+    , state_info as (
+        select * 
+        from {{ ref('stg_adventure_works__stateprovince') }}
+    )
+
+    , country_info as (
+        select * 
+        from {{ ref('stg_adventure_works__countryregion') }}
+    )
+
+    , unioned_address as (
+        select
+            address_info.address_id
+            , address_info.address_line_1
+            , address_info.address_line_2
+            , address_info.city as city_name
+            , address_info.state_province_id
+            , address_info.postal_code
+            , address_info.spatial_location
+            , state_info.state_province_code
+            , state_info.country_region_code
+            , state_info.is_only_state_province_flag
+            , state_info.name as state_name
+            , state_info.territory_id
+            , country_info.name as country_name
+        from address_info
+        left join state_info
+            on address_info.state_province_id = state_info.state_province_id
+        left join country_info 
+            on state_info.country_region_code = country_info.country_region_code
+    )
+
+select * from unioned_address
