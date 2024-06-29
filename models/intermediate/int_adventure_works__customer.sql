6d9886a2df091bb3c2cc2080ab810089c82ea1fb
Henrique Coelho <henriquemacedocoelho@hotmail.com>
2024-06-29 23:49:08 +0000

    Creating stg and int models for dim_customer

---
+++ models/intermediate/int_adventure_works__customer.sql
@@ -0,0 +1,32 @@
+with
+    person_info as (
+        select *
+        from {{ ref('stg_adventure_works__person') }}
+    )
+
+    , customer_info as (
+        select *
+        from {{ ref('stg_adventure_works__customer') }}
+    )
+
+    , customer_person_info as (
+        select
+            person_info.business_entity_id
+            , customer_info.customer_id
+            , customer_info.store_id
+            , person_type
+            , name_style
+            , title
+            , first_name
+            , middle_name
+            , last_name
+            , suffix
+            , email_promotion
+            , additional_contact_info
+            , demographics
+        from customer_info
+        left join person_info
+            on customer_info.person_id = person_info.business_entity_id
+    )
+
+select * from customer_person_info
